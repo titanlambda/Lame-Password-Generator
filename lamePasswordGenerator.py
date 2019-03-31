95b19e3b9f9bc37e88e3ba74e984546a57b6d5c5
Tamaghna <tamaghna.basu@gmail.com>
2019-03-31 12:35:58 +0530

    pushed the code

---
+++ lamePasswordGenerator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# importing "random" for random operations 
+import random 
+
+numberOfPasswordToBeGenerated = 50
+words = ["wordA", "wordB", "wordC", "wordD", "wordE", "wordF", "wordG", "wordH", "wordI", "wordJ"]
+special_characters = ['#', '$', '@', '*', '%']
+startingNumber = 100
+endingNumber = 999	
+
+for x in range(0, numberOfPasswordToBeGenerated):
+	wordIndex = random.randrange(0, len(words), 1)	
+	charIndex = random.randrange(0, len(special_characters), 1)
+	randomNumber = random.randrange(100, 999, 1)
+	password = words[wordIndex] + special_characters[charIndex] + str(randomNumber)
+
+#	print ("A random word from the list is : ",end="") 
+#	print(words[wordIndex])
+
+#	print ("A random char from the list is : ",end="") 
+#	print(characters[charIndex])
+
+#	print ("A random number from range is : ",end="") 
+#	print (number) 
+	
+#	print ("password is : ",end="") 
+	print (password) 
